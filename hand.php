eab2d6cc9a613529874941f4968c7fae9d2c2968
KajiAime <ndekaji@gmail.com>
2022-09-02 20:40:15 +0100

    Richard Confort's little website.

---
+++ hand.php
@@ -0,0 +1,31 @@
+<?php
+$to = 'ngnintedem@richardcomfort.com';
+$subject = 'Email from your website.';
+$name = $_POST['name'];
+$headers = "MIME-Version: 1.0"."\r\n";
+$headers .='Content-type: text/plain; charset: utf-8'."\r\n";
+$headers .= "From: $name\r\n";
+$message = "Name: $name\r\n\r\n";
+$email=filter_input(INPUT_POST,'email',FILTER_VALIDATE_EMAIL);
+if($email){
+    $headers.="Reply_to: $email";
+    $message.="Email: $email\r\n\r\n";
+}
+$message.= "Town: ".$_POST['town']."\r\n\r\n";
+$message.= "Message: ".$_POST['msg'];
+$sent = mail($to,$subject,$message,$headers,'-f'.$to);
+?>
+<html>
+    <head>
+        <title></title>
+        <meta http-equiv="refresh" content="3; URL='contact.htm'"/>
+    </head>
+    <body>
+        <?php if(isset($sent) && $sent){?>
+            <h1><?php echo $name;?> ,thank you for contacting us!</h1>
+            <?php }else{?>
+                <h1>The message was not sent! Please retry to send.</h1>
+            <?php }?>
+            <a href="contact.htm">Click here to go back</a>
+    </body>
+</html>
